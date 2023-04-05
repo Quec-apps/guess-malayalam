8e12787ad56c06c2cb6dea3366f73d855c53007a
Quec-apps <79459289+Quec-apps@users.noreply.github.com>
2023-04-05 22:58:24 +0530

    Add files via upload

---
+++ song/res/song.js
@@ -0,0 +1,372 @@
+b = 0;
+
+b++;
+window["q"+b] = "Mehabooba"; //Ans1
+window["q1"+b] = "Kizhakku Pookkum";
+window["q2"+b] = "Raavu Maayave";
+window["q3"+b] = "Johny Mone Johny";
+window["q4"+b] = "Lailakame ";
+window["li"+b] = 'https://youtu.be/C8ByfT19Xns';
+
+b++;
+window["q"+b] = "Puthiyoru Lokam"; //Ans2
+window["q1"+b] = "Onakka Munthiri";
+window["q2"+b] = "Mehabooba";
+window["q3"+b] = "Uyire";
+window["q4"+b] = "Neelakasham";
+window["li"+b] = 'https://youtu.be/lZL2K3ewRYM';
+
+b++;
+window["q"+b] = "Payye Veeshum Kaatil"; //Ans3
+window["q1"+b] = "Sita Kalyanam";
+window["q2"+b] = "Ee Shishirakaalam";
+window["q3"+b] = "Thaniye Mizhikal";
+window["q4"+b] = "Wow";
+window["li"+b] = 'https://youtu.be/s_lRDM3Ft_I';
+
+b++;
+window["q"+b] = "Onakka Munthiri"; //Ans4
+window["q1"+b] = "Appangal Eambadum";
+window["q2"+b] = "Kizhakku Pookkum";
+window["q3"+b] = "Enthaavo";
+window["q4"+b] = "Aalolam";
+window["li"+b] = 'https://youtu.be/wya5utKP_Po';
+
+b++;
+window["q"+b] = "Uyire"; //Ans5
+window["q1"+b] = "Aaradhike";
+window["q2"+b] = "Thumbi Penne";
+window["q3"+b] = "Aaro Nenjil";
+window["q4"+b] = "Puthiyoru Lokam";
+window["li"+b] = 'https://youtu.be/Fd7-FybJlbs';
+
+b++;
+window["q"+b] = "Darshana"; //Ans6
+window["q1"+b] = "Etho Mazhayil";
+window["q2"+b] = "Sneham Cherum Neram";
+window["q3"+b] = "Raavu Maayave";
+window["q4"+b] = "Vennilave Ninnarikil";
+window["li"+b] = 'https://youtu.be/epAFDEJImrU';
+
+b++;
+window["q"+b] = "Manikya Malaraya Poovi"; //Ans7
+window["q1"+b] = "Mehabooba";
+window["q2"+b] = "Aadyam Thammil";
+window["q3"+b] = "Aaradhike";
+window["q4"+b] = "Thoominnal";
+window["li"+b] = 'https://youtu.be/W0fKl43QmIE';
+
+b++;
+window["q"+b] = "Kannil Kannil"; //Ans8
+window["q1"+b] = "Malare";
+window["q2"+b] = "Etho Mazhayil";
+window["q3"+b] = "Aaro Nenjil";
+window["q4"+b] = "Payye Veeshum Kaatil";
+window["li"+b] = 'https://youtu.be/DlXK47snKp0';
+
+b++;
+window["q"+b] = "Raavu Maayave"; //Ans9
+window["q1"+b] = "Nilaavil Ellame";
+window["q2"+b] = "Aadyam Thammil";
+window["q3"+b] = "Uyire";
+window["q4"+b] = "Kaikkottum Kandittilla";
+window["li"+b] = 'https://youtu.be/QQTSs0qeHyw';
+
+b++;
+window["q"+b] = "Aalolam"; //Ans10
+window["q1"+b] = "Johny Mone Johny";
+window["q2"+b] = "Aaro Nenjil";
+window["q3"+b] = "Thoominnal";
+window["q4"+b] = "Onakka Munthiri";
+window["li"+b] = 'https://youtu.be/CfTWu4Llbvo';
+
+b++;
+window["q"+b] = "Lailakame "; //Ans11
+window["q1"+b] = "Uyiril Thodum";
+window["q2"+b] = "Neeyo Njaano";
+window["q3"+b] = "Njan Kanavil";
+window["q4"+b] = "Appangal Eambadum";
+window["li"+b] = 'https://youtu.be/FAn2i7gu32w';
+
+b++;
+window["q"+b] = "Malare"; //Ans12
+window["q1"+b] = "Aaradhike";
+window["q2"+b] = "Aalolam";
+window["q3"+b] = "Aadyam Thammil";
+window["q4"+b] = "Aaradhike";
+window["li"+b] = 'https://youtu.be/0G2VxhV_gXM';
+
+b++;
+window["q"+b] = "Aluva Puzha"; //Ans13
+window["q1"+b] = "Malare";
+window["q2"+b] = "Malare";
+window["q3"+b] = "Uyiril Thodum";
+window["q4"+b] = "Thumbi Penne";
+window["li"+b] = 'https://youtu.be/fiAlruU48HI';
+
+b++;
+window["q"+b] = "Ethu Kari Raavilum"; //Ans14
+window["q1"+b] = "Manogatham Bhavan";
+window["q2"+b] = "Kannil Kannil";
+window["q3"+b] = "Kizhakku Pookkum";
+window["q4"+b] = "Sita Kalyanam";
+window["li"+b] = 'https://youtu.be/dz3ik3XzR54';
+
+b++;
+window["q"+b] = "Mel Mel"; //Ans15
+window["q1"+b] = "Manogatham Bhavan";
+window["q2"+b] = "Aaradhike";
+window["q3"+b] = "Wow";
+window["q4"+b] = "Payye Veeshum Kaatil";
+window["li"+b] = 'https://youtu.be/TXnH6-2HPqE';
+
+b++;
+window["q"+b] = "Thaniye Mizhikal"; //Ans16
+window["q1"+b] = "Aalolam";
+window["q2"+b] = "Manikya Malaraya Poovi";
+window["q3"+b] = "Njan Kanavil";
+window["q4"+b] = "Appangal Eambadum";
+window["li"+b] = 'https://youtu.be/l0yU-mcCM9I';
+
+b++;
+window["q"+b] = "Aaradhike"; //Ans17
+window["q1"+b] = "Darshana";
+window["q2"+b] = "Roshomon";
+window["q3"+b] = "Raavu Maayave";
+window["q4"+b] = "Vennilave Ninnarikil";
+window["li"+b] = 'https://youtu.be/dAezp422I_A';
+
+b++;
+window["q"+b] = "Dooreyo"; //Ans18
+window["q1"+b] = "Thaniye Mizhikal";
+window["q2"+b] = "Neelakasham";
+window["q3"+b] = "Mel Mel";
+window["q4"+b] = "Kaikkottum Kandittilla";
+window["li"+b] = 'https://youtu.be/bRYaCXVW7Hg';
+
+b++;
+window["q"+b] = "Uyiril Thodum"; //Ans19
+window["q1"+b] = "Ethu Kari Raavilum";
+window["q2"+b] = "Thaniye Mizhikal";
+window["q3"+b] = "Mehabooba";
+window["q4"+b] = "Johny Mone Johny";
+window["li"+b] = 'https://youtu.be/ZKhOs_Pc_7s';
+
+b++;
+window["q"+b] = "Parayuvaan"; //Ans20
+window["q1"+b] = "Kalippu";
+window["q2"+b] = "Kalippu";
+window["q3"+b] = "Darshana";
+window["q4"+b] = "Madhu Pole";
+window["li"+b] = 'https://youtu.be/IWOPRFRNj3g';
+
+b++;
+window["q"+b] = "Uyirin Nadhiye"; //Ans21
+window["q1"+b] = "Manogatham Bhavan";
+window["q2"+b] = "Puthiyoru Lokam";
+window["q3"+b] = "Kalippu";
+window["q4"+b] = "Etho Mazhayil";
+window["li"+b] = 'https://youtu.be/4q5o3Tiwcmc';
+
+b++;
+window["q"+b] = "Neelakasham"; //Ans22
+window["q1"+b] = "Mehabooba";
+window["q2"+b] = "Thumbi Penne";
+window["q3"+b] = "Sneham Cherum Neram";
+window["q4"+b] = "Uyiril Thodum";
+window["li"+b] = 'https://youtu.be/RmyrkNCFMZk';
+
+b++;
+window["q"+b] = "Sita Kalyanam"; //Ans23
+window["q1"+b] = "Aluva Puzha";
+window["q2"+b] = "Aaradhike";
+window["q3"+b] = "Neelakasham";
+window["q4"+b] = "Vennilave Ninnarikil";
+window["li"+b] = 'https://youtu.be/LRHSq0tTLB0';
+
+b++;
+window["q"+b] = "Roshomon"; //Ans24
+window["q1"+b] = "Madhu Pole";
+window["q2"+b] = "Ethu Kari Raavilum";
+window["q3"+b] = "Enthaavo";
+window["q4"+b] = "Vennilave Ninnarikil";
+window["li"+b] = 'https://youtu.be/d80VayL1r20';
+
+b++;
+window["q"+b] = "Thumbi Penne"; //Ans25
+window["q1"+b] = "Onakka Munthiri";
+window["q2"+b] = "Kizhakku Pookkum";
+window["q3"+b] = "Thoominnal";
+window["q4"+b] = "Aluva Puzha";
+window["li"+b] = 'https://youtu.be/elBVykcQ8Qo';
+
+b++;
+window["q"+b] = "Thudakam Mangalyam"; //Ans26
+window["q1"+b] = "Kannil Kannil";
+window["q2"+b] = "Lailakame ";
+window["q3"+b] = "Aaradhike";
+window["q4"+b] = "Aaro Nenjil";
+window["li"+b] = 'https://youtu.be/a3UNpjnYquI';
+
+b++;
+window["q"+b] = "Appangal Eambadum"; //Ans27
+window["q1"+b] = "Mel Mel";
+window["q2"+b] = "Enthaavo";
+window["q3"+b] = "Wow";
+window["q4"+b] = "Malare";
+window["li"+b] = 'https://youtu.be/QcESg-9Vfzo';
+
+b++;
+window["q"+b] = "Johny Mone Johny"; //Ans28
+window["q1"+b] = "Uyire";
+window["q2"+b] = "Sita Kalyanam";
+window["q3"+b] = "Darshana";
+window["q4"+b] = "Uyire";
+window["li"+b] = 'https://youtu.be/YMcC95uFVCs';
+
+b++;
+window["q"+b] = "Kaikkottum Kandittilla"; //Ans29
+window["q1"+b] = "Aadyam Thammil";
+window["q2"+b] = "Lailakame ";
+window["q3"+b] = "Etho Mazhayil";
+window["q4"+b] = "Payye Veeshum Kaatil";
+window["li"+b] = 'https://youtu.be/UzqZbwljXzs';
+
+b++;
+window["q"+b] = "Kalippu"; //Ans30
+window["q1"+b] = "Kannil Kannil";
+window["q2"+b] = "Neelakasham";
+window["q3"+b] = "Johny Mone Johny";
+window["q4"+b] = "Malare";
+window["li"+b] = 'https://youtu.be/PeUjir1kMFM';
+
+b++;
+window["q"+b] = "Vennilave Ninnarikil"; //Ans31
+window["q1"+b] = "Johny Mone Johny";
+window["q2"+b] = "Thumbi Penne";
+window["q3"+b] = "Parayuvaan";
+window["q4"+b] = "Mel Mel";
+window["li"+b] = 'https://youtu.be/3w_iOdYnoSw';
+
+b++;
+window["q"+b] = "Etho Mazhayil"; //Ans32
+window["q1"+b] = "Thumbi Penne";
+window["q2"+b] = "Enthaavo";
+window["q3"+b] = "Aalolam";
+window["q4"+b] = "Aalolam";
+window["li"+b] = 'https://youtu.be/Yuy3BI6wmPU';
+
+b++;
+window["q"+b] = "Madhu Pole"; //Ans33
+window["q1"+b] = "Ee Shishirakaalam";
+window["q2"+b] = "Mel Mel";
+window["q3"+b] = "Mehabooba";
+window["q4"+b] = "Mazhamegham";
+window["li"+b] = 'https://youtu.be/ZbwnKmX0ygc';
+
+b++;
+window["q"+b] = "Mazhamegham"; //Ans34
+window["q1"+b] = "Neeyo Njaano";
+window["q2"+b] = "Thumbi Penne";
+window["q3"+b] = "Kizhakku Pookkum";
+window["q4"+b] = "Enthaavo";
+window["li"+b] = 'https://youtu.be/Os9TlUlCkX0';
+
+b++;
+window["q"+b] = "Ee Shishirakaalam"; //Ans35
+window["q1"+b] = "Sneham Cherum Neram";
+window["q2"+b] = "Nilaavil Ellame";
+window["q3"+b] = "Manogatham Bhavan";
+window["q4"+b] = "Mehabooba";
+window["li"+b] = 'https://youtu.be/JEeEApK-XqE';
+
+b++;
+window["q"+b] = "Neeyo Njaano"; //Ans36
+window["q1"+b] = "Njan Kanavil";
+window["q2"+b] = "Enthaavo";
+window["q3"+b] = "Vennilave Ninnarikil";
+window["q4"+b] = "Darshana";
+window["li"+b] = 'https://youtu.be/YLDuLtNSLAI';
+
+b++;
+window["q"+b] = "Manogatham Bhavan"; //Ans37
+window["q1"+b] = "Sneham Cherum Neram";
+window["q2"+b] = "Madhu Pole";
+window["q3"+b] = "Aaradhike";
+window["q4"+b] = "Aaradhike";
+window["li"+b] = 'https://youtu.be/2uTNC3V444o';
+
+b++;
+window["q"+b] = "Aadyam Thammil"; //Ans38
+window["q1"+b] = "Mehabooba";
+window["q2"+b] = "Dooreyo";
+window["q3"+b] = "Kizhakku Pookkum";
+window["q4"+b] = "Kalippu";
+window["li"+b] = 'https://youtu.be/RNbRM2NsGt8';
+
+b++;
+window["q"+b] = "Sneham Cherum Neram"; //Ans39
+window["q1"+b] = "Vennilave Ninnarikil";
+window["q2"+b] = "Thumbi Penne";
+window["q3"+b] = "Vennilave Ninnarikil";
+window["q4"+b] = "Aalolam";
+window["li"+b] = 'https://youtu.be/DFZ6eram8Fo';
+
+b++;
+window["q"+b] = "Enthaavo"; //Ans40
+window["q1"+b] = "Kannil Kannil";
+window["q2"+b] = "Nilaavil Ellame";
+window["q3"+b] = "Mel Mel";
+window["q4"+b] = "Darshana";
+window["li"+b] = 'https://youtu.be/xx4TFd4-wz8';
+
+b++;
+window["q"+b] = "Nilaavil Ellame"; //Ans41
+window["q1"+b] = "Mel Mel";
+window["q2"+b] = "Raavu Maayave";
+window["q3"+b] = "Payye Veeshum Kaatil";
+window["q4"+b] = "Raavu Maayave";
+window["li"+b] = 'https://youtu.be/FVwTyWbW64M';
+
+b++;
+window["q"+b] = "Wow"; //Ans42
+window["q1"+b] = "Aaro Nenjil";
+window["q2"+b] = "Mazhamegham";
+window["q3"+b] = "Mehabooba";
+window["q4"+b] = "Neelakasham";
+window["li"+b] = 'https://youtu.be/39cFfvYqsQw';
+
+b++;
+window["q"+b] = "Aaro Nenjil"; //Ans43
+window["q1"+b] = "Puthiyoru Lokam";
+window["q2"+b] = "Ethu Kari Raavilum";
+window["q3"+b] = "Ethu Kari Raavilum";
+window["q4"+b] = "Lailakame ";
+window["li"+b] = 'https://youtu.be/UsJkhu8K1gs';
+
+b++;
+window["q"+b] = "Thoominnal"; //Ans44
+window["q1"+b] = "Dooreyo";
+window["q2"+b] = "Madhu Pole";
+window["q3"+b] = "Kaikkottum Kandittilla";
+window["q4"+b] = "Kaikkottum Kandittilla";
+window["li"+b] = 'https://youtu.be/rxaqSsTQeGQ';
+
+b++;
+window["q"+b] = "Kizhakku Pookkum"; //Ans45
+window["q1"+b] = "Aaradhike";
+window["q2"+b] = "Ee Shishirakaalam";
+window["q3"+b] = "Uyiril Thodum";
+window["q4"+b] = "Puthiyoru Lokam";
+window["li"+b] = 'https://youtu.be/YrLRhuim5aM';
+
+b++;
+window["q"+b] = "Njan Kanavil"; //Ans46
+window["q1"+b] = "Aaro Nenjil";
+window["q2"+b] = "Etho Mazhayil";
+window["q3"+b] = "Uyirin Nadhiye";
+window["q4"+b] = "Kalippu";
+window["li"+b] = 'https://youtu.be/oW0lgLqqoNE';
+
+
+songLevelTotal = b;
